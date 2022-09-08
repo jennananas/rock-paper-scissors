f43305eb1bc1ad02dcf3aac5d6b4de71b680a45d
jennananas <jennarj97@gmail.com>
2022-09-08 17:45:39 +0200

    Add function playARound()

--- project.js
+++ project.js
@@ -11,3 +11,51 @@ function getPlayerChoice(){
     }
     return(playerChoice);
 }
+
+
+
+function playARound(playerSelection, computerSelection) {
+    let result, winnerSelection, loserSelection = "" ;
+    if (playerSelection==computerSelection){
+        return ("it's a tie !");
+    } else if (playerSelection=="paper"){
+        if (computerSelection=="rock") {
+            result = "win";
+            winnerSelection=playerSelection;
+            loserSelection = computerSelection;
+        }
+        else {
+            result = "lose";
+            loserSelection = playerSelection;
+            winnerSelection=computerSelection;}
+
+    } 
+    else if (playerSelection=="rock"){
+        if (computerSelection=="scissors"){
+            result = "win";
+            winnerSelection=playerSelection;
+            loserSelection = computerSelection;
+        }
+        else 
+        {
+            result = "lose";
+            loserSelection = playerSelection;
+            winnerSelection=computerSelection;}
+    }
+    else if (playerSelection=="scissors"){
+        if (computerSelection=="paper"){
+            result = "win";
+            loserSelection = computerSelection;
+            winnerSelection=playerSelection;
+        } else 
+        {
+            result ="lose";
+            loserSelection = playerSelection;
+            winnerSelection=computerSelection;
+        }
+    }
+    return(`You ${result} ! ${winnerSelection} beats ${loserSelection}`);
+}
+
+const playerSelection = getPlayerChoice();
+const computerSelection = getComputerChoice();
