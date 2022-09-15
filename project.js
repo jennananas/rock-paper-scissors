ed20c2094fc7bcc1bab2f162c0b35986c4088b93
jennananas <jennarj97@gmail.com>
2022-09-15 15:31:18 +0200

    add buttons for player ; change playRound to trigger it with buttons ; add result on html

--- project.js
+++ project.js
@@ -1,30 +1,39 @@
 const choices = ["rock", "paper", "scissors"] ;
+const buttons = document.querySelectorAll("input");
+let resultText = document.querySelector("#result");
+let roundNb = playerScore = computerScore = 0;
 
+
+/* Retourne au hasard le choix de l'ordi parmi [rock, paper, scissors] */
 function getComputerChoice(){
     randomChoice = Math.floor(Math.random() * choices.length);
     return (choices[randomChoice]);
 }
 
-function getPlayerChoice(){
-    let playerChoice = "";
-    while (!(choices.includes(playerChoice))){
-        playerChoice = prompt("Rock, Paper or Scissors ?").toLowerCase();
+/* Désactive les boutons */
+function stopGame(){
+    buttons.forEach(button  => {
+        button.disabled = true});
     }
-    return(playerChoice);
-}
+    
 
-function playARound(playerSelection, computerSelection) {
-    let result, winnerSelection, loserSelection = "" ;
+/* Joue un round selon le choix du joueur, retourne les scores et le vainqueur à l'issue des 5 rounds */
+function playRound(playerSelection) {
+    let computerSelection = getComputerChoice();
+    let result, winnerSelection, loserSelection;
+    result, winnerSelection = loserSelection = "";
     if (playerSelection==computerSelection){
         return ("it's a tie !");
     } else if (playerSelection=="paper"){
         if (computerSelection=="rock") {
             result = "win";
+            playerScore+= 1;
             winnerSelection=playerSelection;
             loserSelection = computerSelection;
         }
         else {
             result = "lose";
+            computerScore+= 1;
             loserSelection = playerSelection;
             winnerSelection=computerSelection;}
 
@@ -32,36 +41,56 @@ function playARound(playerSelection, computerSelection) {
     else if (playerSelection=="rock"){
         if (computerSelection=="scissors"){
             result = "win";
+            playerScore +=1;
             winnerSelection=playerSelection;
             loserSelection = computerSelection;
         }
         else 
         {
             result = "lose";
+            computerScore+= 1;
             loserSelection = playerSelection;
             winnerSelection=computerSelection;}
     }
     else if (playerSelection=="scissors"){
         if (computerSelection=="paper"){
             result = "win";
+            playerScore +=1;
             loserSelection = computerSelection;
             winnerSelection=playerSelection;
         } else 
         {
             result ="lose";
+            computerScore+= 1;
             loserSelection = playerSelection;
             winnerSelection=computerSelection;
         }
     }
-    return(`You ${result} ! ${winnerSelection} beats ${loserSelection}`);
+
+    roundNb++;
+
+    if (playerScore === 5) {
+        winner = "YOU"; 
+    } else {
+        winner = "COMPUTER"; 
+    }
+
+    resultText.innerHTML = `Round ${roundNb}<br>
+    choix joueur : ${playerSelection}<br>
+    choix ordi : ${computerSelection}<br>
+    score joueur : ${playerScore}<br>
+    score ordi : ${computerScore}`
+
+    if (playerScore === 5 || computerScore === 5) {
+        stopGame();
+        resultText.innerHTML = `${winner} win ! Final score : ${playerScore}-${computerScore}`
+    }
+   
+    return resultText;
+
 }
 
-function game(numberOfRound){
-    let result = "";
-    numberOfRound=Number(prompt("How many rounds would you like to play ?"));
-    for (let i=0; i<numberOfRound; i++){
-        result = result + `Game ${i} : ${playARound(getPlayerChoice(), getComputerChoice())}` + "\n";
-    }
-    return (result);
-}
-console.log(game());
+/* Déclenche le round à chaque clic */
+buttons.forEach((button) => button.addEventListener("click", () => {
+    playRound(button.value);
+}))
